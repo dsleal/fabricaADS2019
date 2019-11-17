abbc702bb1157b9619ea9a19590863dfb5e73f54
diogo <dt1.leal@gmail.com>
2019-11-17 17:51:41 -0300

    Inserido o alterar / editar

--- app/src/main/java/contrato/com/activities/administrador/EditTipoPrestador.java
+++ app/src/main/java/contrato/com/activities/administrador/EditTipoPrestador.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import contrato.com.R;
@@ -19,6 +21,12 @@ import retrofit2.Retrofit;
 public class EditTipoPrestador extends AppCompatActivity {
     EditText codigo;
     EditText descricao;
+    RadioButton rbEditHabilitado;
+    RadioButton rbEditDesabilitado;
+    RadioGroup rbEditGroup;
+    int id;
+    Boolean desabilitado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +35,31 @@ public class EditTipoPrestador extends AppCompatActivity {
 
         codigo = findViewById(R.id.txtEditCod);
         descricao = findViewById(R.id.txtEditTpDesc);
+        rbEditDesabilitado = findViewById(R.id.rbEditDesabilitado);
+        rbEditHabilitado = findViewById(R.id.rbEditHabilitado);
         Intent intent = getIntent();
-        int id = intent.getIntExtra("ID",0);
-
+        id = intent.getIntExtra("ID",0);
 
         Retrofit retrofit = APIClient.getClient();
         TipoPrestadorResource tipoPrestador = retrofit.create(TipoPrestadorResource.class);
-        Call<TipoPrestador> get = tipoPrestador.get(id);
+        Call<TipoPrestador> get = tipoPrestador.getPorId(id);
         get.enqueue(new Callback<TipoPrestador>() {
             @Override
             public void onResponse(Call<TipoPrestador> call, Response<TipoPrestador> response) {
-                TipoPrestador tp = new TipoPrestador();
-                codigo.setText(tp.getId());
+                TipoPrestador tp = response.body();
+                codigo.setText(Integer.toString(tp.getId()));
                 descricao.setText(tp.getDescricao());
+                if(tp.isDesativado()){
+                    rbEditDesabilitado.setChecked(true);
+                    rbEditHabilitado.setChecked(false);
+                }else{
+                    rbEditDesabilitado.setChecked(false);
+                    rbEditHabilitado.setChecked(true);
+                }
             }
 
             @Override
             public void onFailure(Call<TipoPrestador> call, Throwable t) {
-                Toast.makeText(EditTipoPrestador.this, "Tipo de prestador com código '" + Integer.toString(id) + "' nao encontrado!", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -52,7 +67,58 @@ public class EditTipoPrestador extends AppCompatActivity {
     }
 
     public void alterar(View view){
+        Retrofit retrofit = APIClient.getClient();
+        TipoPrestadorResource tipoPrestador = retrofit.create(TipoPrestadorResource.class);
+        TipoPrestador tp = new TipoPrestador();
+        tp.setDescricao(descricao.getText().toString());
+        tp.setId(id);
+
+        //Identificar radio button selecionado
+        rbEditGroup = findViewById(R.id.rbEditGroup);
+        int selectedRadioButtonID = rbEditGroup.getCheckedRadioButtonId();
+        RadioButton selectedRadioButton = (RadioButton) findViewById(selectedRadioButtonID);
+        String selectedRadioButtonText = selectedRadioButton.getText().toString();
+
+        if(selectedRadioButtonText.equals("Desabilitado")){
+            desabilitado = true;
+        }
+        else if (selectedRadioButtonText.equals("Habilitado")){
+            desabilitado = false;
+        }
+        tp.setDesativado(desabilitado);
+        Call<TipoPrestador> alterar = tipoPrestador.put(id, tp);
+        alterar.enqueue(new Callback<TipoPrestador>() {
+            @Override
+            public void onResponse(Call<TipoPrestador> call, Response<TipoPrestador> response) {
+                TipoPrestador tPrest = response.body();
+                Toast.makeText(getBaseContext(), "Tipo de Prestador alterado com sucesso!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(EditTipoPrestador.this, TTipoPrestador.class));
+            }
+
+            @Override
+            public void onFailure(Call<TipoPrestador> call, Throwable t) {
+                Toast.makeText(getBaseContext(), "Não foi possível alterar o tipo de prestador!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
-    public void remover(View view){}
+    public void remover(View view){
+
+        Retrofit retrofit = APIClient.getClient();
+        TipoPrestadorResource tipoPrestador = retrofit.create(TipoPrestadorResource.class);
+        Call<Void> delete = tipoPrestador.delete(id);
+        delete.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(getBaseContext(), "Tipo de Prestador removido com sucesso!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(EditTipoPrestador.this, TTipoPrestador.class));
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(getBaseContext(), "Não foi possível remover o tipo de prestador!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
